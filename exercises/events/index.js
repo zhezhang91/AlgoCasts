c449c181d861b9bef7b8902b5a9d21c6dc7396e6
zhezhang91 <zz188659@gmail.com>
2019-07-04 11:11:49 -0400

    finish tree

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,29 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    this.events[eventName]
+      ? this.events[eventName].push(callback)
+      : (this.events[eventName] = [callback]);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let callback of this.events[eventName]) callback();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
